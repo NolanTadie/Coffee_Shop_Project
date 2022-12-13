aa83f8ba7ce29206195f5219d92dbb89d55e817e
NolanTadie <120289246+NolanTadie@users.noreply.github.com>
2022-12-13 17:48:17 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+# need to import Coffee ASCII logo
+
+print("Hello, welcome to NetworkChuck Coffee Shop!")
+name = input("What's your name for the order?\n")
+print(f"Hello {name}, thank you so much for coming in today!\n")
+
+menu = "$2.00 - Black Coffee\n" + "$2:50 - Espresso\n" + "$3:00 - Latte\n" "$3:50 - Cappucino\n"
+
+print(f"What would you like from our menu today {name}? Here is what we're serving.\n {menu}")
+
+# if order = input():
+# if order():
+
+price = 8
+quantity = input("How many " + order + "'s would you like?\n")
+total = int(price) * int(quantity)
+
+print(f"Sounds good {name}, we'll have that {order} ready for you in a moment.")
+print("The total for your bill is $" + str(total))
+# black_coffee = float(2.00)
+# espresso = float(2.50)
+# latte = float(3.00)
+# cappucino = float(3.50)
+#
+#
+# print("Sounds good {name}, we'll have that {order}")
